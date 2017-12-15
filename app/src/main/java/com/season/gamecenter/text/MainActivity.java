5f615dc5e114a10624aa42e32947fadb6cf23285
451360508@qq.com <451360508@qq.com>
2017-12-15 11:33:56 +0800

    图片接收

--- app/src/main/java/com/season/gamecenter/text/MainActivity.java
+++ app/src/main/java/com/season/gamecenter/text/MainActivity.java
@@ -2,22 +2,67 @@ package com.season.gamecenter.text;
 
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 import com.season.gamecenter.R;
+import com.season.gamecenter.ball.single.LogConsole;
 
-public class MainActivity extends Activity implements OnClickListener{
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
 
+        imageView = findViewById(R.id.gameView);
 
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+        LogConsole.log("type = "+type);
+        //Bitmap too large to be uploaded into a texture (4160x3120, max=4096x4096)
+        if (action.equals(Intent.ACTION_SEND) && type.equals("image/*")) {
+            Uri uri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+            LogConsole.log("uri = "+uri);
+            if (uri != null) {
+                try {
+                    LogConsole.log("uri.getPath() = "+uri.getPath());
+                    FileInputStream fileInputStream = new FileInputStream(uri.getPath());
+                    Bitmap bitmap = BitmapFactory.decodeStream(fileInputStream);
+                    imageView.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
-
-        setContentView(R.layout.activity_main);
+        if (action.equals(Intent.ACTION_SEND_MULTIPLE) && type.equals("image/*")) {
+            ArrayList<Uri> uris=intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+            LogConsole.log("uri = "+ uris.size());
+            if (uris != null && uris.size() > 0) {
+                try {
+                    LogConsole.log("uri.getPath() = "+ uris.get(0).getPath());
+                    FileInputStream fileInputStream = new FileInputStream(uris.get(0).getPath());
+                    Bitmap bitmap = BitmapFactory.decodeStream(fileInputStream);
+                    imageView.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
